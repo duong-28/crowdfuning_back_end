dce9df98b9795935228159b66ac6980ada6ab7ca
LucyNguyen <thuyduong.kt.ftu@gmail.com>
2024-10-29 00:48:17 +1000

    debug set to True

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
